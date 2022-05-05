421f0507d429d9edc607a2a1d58ea009ac2b765d
dinomaster186 <peterwetherell186@gmail.com>
2022-05-04 22:30:05 -0700

    10:29 pm 05/04/22 Implemented system such that the loop times should be relatively constant and settable Implemented localizer Implemented system where second control hub is only called when needed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleMecanumDrive.java
@@ -8,7 +8,6 @@ import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
@@ -38,6 +37,29 @@ public class SampleMecanumDrive {
 
     int[] encoders;
 
+    double currentSlideLength = 0;
+    double currentSlideSpeed = 0;
+    double currentTurretAngle = 0;
+    double currentIntakeSpeed = 0;
+
+    int rightIntakeVal = 0;
+    int leftIntakeVal = 0;
+    int depositVal = 0;
+    int flexSensorVal = 0;
+    double distValLeft = 0;
+    double distValRight = 0;
+    int magValLeft = 0;
+    int magValRight = 0;
+
+    boolean updateHub2 = false;
+
+    private long loopStart = System.nanoTime();
+    double loopTime = 0.0;
+
+    public ArrayList<UpdatePriority> motorPriorities;
+
+    Localizer localizer;
+
     public void initMotors(HardwareMap hardwareMap){
         expansionHub1 = hardwareMap.get(ExpansionHubEx.class, "Expansion Hub 1");
         leftFront = (ExpansionHubMotor) hardwareMap.dcMotor.get("lf");
@@ -53,6 +75,13 @@ public class SampleMecanumDrive {
 
         setDriveMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         setDriveMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        for (int i = 0; i < 4; i ++) {
+            motorPriorities.add(new UpdatePriority(3,5));
+        }
+        motorPriorities.add(new UpdatePriority(1,2));
+        motorPriorities.add(new UpdatePriority(1,3));
+        motorPriorities.add(new UpdatePriority(2,6));
 
         motors = Arrays.asList(leftFront, leftBack, rightBack, rightFront, intake, turret, slides, slides2);
     }
@@ -111,23 +140,54 @@ public class SampleMecanumDrive {
         initServos(hardwareMap);
         initMotors(hardwareMap);
         initSensors(hardwareMap);
+        localizer = new Localizer();
+    }
+    public void pinMotorPowers (double v, double v1, double v2, double v3) {
+        motorPriorities.get(0).setTargetPower(v);
+        motorPriorities.get(1).setTargetPower(v1);
+        motorPriorities.get(2).setTargetPower(v2);
+        motorPriorities.get(3).setTargetPower(v3);
     }
     public void update(){
+        getEncoders(); //This is the one thing that is guaranteed to occur every loop because we need encoders for odo
 
-    }public void getEncoders(){
+        loopTime = (System.nanoTime() - loopStart) / 1000000000.0; //gets the current time since the loop began
+        double targetLoopLength = 0.01; //Sets the target loop time in seconds
+        double a = 1;
+        while(loopTime <= targetLoopLength && a >= 0){ // updates the motors while still time remaining in the loop
+            int bestIndex = 0;
+            double bestScore = motorPriorities.get(0).getPriority();
+            for (int i = 1; i < motorPriorities.size(); i ++){ //finding the motor that is most in need of being updated;
+                if (motorPriorities.get(i).getPriority() > bestScore){
+                    bestIndex = i;
+                    bestScore = motorPriorities.get(i).getPriority();
+                }
+            }
+            motors.get(bestIndex).setPower(motorPriorities.get(bestIndex).power); //setting the motor of the one that most needs it
+            if (bestIndex == motorPriorities.size()-1){
+                slides2.setPower(motorPriorities.get(bestIndex).power); //This deals with the case of the linked mechanism for the slides. If one gets chosen the other must also be chosen
+            }
+            motorPriorities.get(bestIndex).update(); //Resetting the motor priority so that it knows that it updated the motor
+            loopTime = (System.nanoTime() - loopStart) / 1000000000.0;
+            a = bestScore; //Checking to make sure a motor was updated because if it wasn't then can move onto next thing
+        }
+
+        updateHub2 = false;
+        loopStart = System.nanoTime();
+    }
+    public void getEncoders(){
         bulkData = expansionHub1.getBulkInputData();
         if (bulkData != null) {
             try {
                 encoders[0] = bulkData.getMotorCurrentPosition(rightFront);
                 encoders[1] = bulkData.getMotorCurrentPosition(leftFront);
-                encoders[2] = bulkData.getMotorCurrentPosition(rightRear);
-                intakeSpeed = ((double) bulkData.getMotorVelocity(leftRear)) / intakeTicksPerRev;
-                if (encoders.length == 4) {
-                    encoders[3] = bulkData.getMotorCurrentPosition(leftRear);
-                }
+                encoders[2] = bulkData.getMotorCurrentPosition(rightBack);
+                localizer.updateEncoders(encoders);
+                localizer.update();
+                currentIntakeSpeed = ((double) bulkData.getMotorVelocity(leftBack)) / (((1.0+(46.0/11.0)) * 28.0) / (26.0/19.0));
                 rightIntakeVal = bulkData.getAnalogInputValue(rightIntake);
                 leftIntakeVal = bulkData.getAnalogInputValue(leftIntake);
-                depositVal = bulkData.getAnalogInputValue(depositSensor); //Math.pow(2,(double)bulkData.getAnalogInputValue(depositSensor)/1000.0);
+                depositVal = bulkData.getAnalogInputValue(depositSensor);
                 flexSensorVal = bulkData.getAnalogInputValue(flex);
             }
             catch (Exception e){
@@ -136,24 +196,26 @@ public class SampleMecanumDrive {
                 Log.e("******* fail", "control hub failed");
             }
         }
+    }
+    public void updateHub2(){
+        if (!updateHub2) {
+            updateHub2 = true;
+            RevBulkData bulkData = expansionHub2.getBulkInputData();
+            if (bulkData != null) {
+                try {
+                    currentSlideLength = bulkData.getMotorCurrentPosition(slides2) / 25.1372713591;
+                    currentSlideSpeed = bulkData.getMotorVelocity(slides2) / 25.1372713591;
+                    currentTurretAngle = bulkData.getMotorCurrentPosition(turret) / 578.3213;
+                    distValLeft = bulkData.getAnalogInputValue(distLeft) / 3.2;
+                    distValRight = bulkData.getAnalogInputValue(distRight) / 3.2;
+                    magValLeft = bulkData.getAnalogInputValue(magLeft);
+                    magValRight = bulkData.getAnalogInputValue(magRight);
 
-        // you can set the bulkData to the other expansion hub to get data from the other one
-        RevBulkData bulkData = expansionHub2.getBulkInputData();
-        if (bulkData != null) {
-            try {
-                slideExtensionLength = bulkData.getMotorCurrentPosition(slides2) / slideTickToInch;
-                currentSlidesSpeed = bulkData.getMotorVelocity(slides2) / slideTickToInch;
-                deleteLater = bulkData.getMotorCurrentPosition(slides) / slideTickToInch;
-                turretHeading = bulkData.getMotorCurrentPosition(turret) / turretTickToRadians;
-                distValLeft = bulkData.getAnalogInputValue(distLeft) / 3.2;
-                distValRight = bulkData.getAnalogInputValue(distRight) / 3.2;
-                magValLeft = bulkData.getAnalogInputValue(magLeft);
-                magValRight = bulkData.getAnalogInputValue(magRight);
-            }
-            catch (Exception e){
-                Log.e("******* Error due to ",e.getClass().getName());
-                e.printStackTrace();
-                Log.e("******* fail", "expansion hub failed");
+                } catch (Exception e) {
+                    Log.e("******* Error due to ", e.getClass().getName());
+                    e.printStackTrace();
+                    Log.e("******* fail", "expansion hub failed");
+                }
             }
         }
     }
@@ -162,5 +224,36 @@ public class SampleMecanumDrive {
         leftBack.setPower(v1);
         rightBack.setPower(v2);
         rightFront.setPower(v3);
+    }
+
+
+    //Lots of getters in order to ensure that the program updates the expansion hub
+    public double getSlideLength(){
+        updateHub2();
+        return currentSlideLength;
+    }
+    public double getSlideSpeed(){
+        updateHub2();
+        return currentSlideSpeed;
+    }
+    public double getTurretAngle(){
+        updateHub2();
+        return currentTurretAngle;
+    }
+    public double getDistValLeft() {
+        updateHub2();
+        return distValLeft;
+    }
+    public double getDistValRight() {
+        updateHub2();
+        return distValRight;
+    }
+    public int getMagValLeft() {
+        updateHub2();
+        return magValLeft;
+    }
+    public int getMagValRight() {
+        updateHub2();
+        return magValRight;
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UpdatePriority.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+public class UpdatePriority {
+    double basePriority;
+    double priorityScale;
+    double lastPower = 0;
+    public double power = 0;
+    long lastUpdateTime;
+    public UpdatePriority(double basePriority, double priorityScale){
+        this.basePriority = basePriority;
+        this.priorityScale = priorityScale;
+        lastUpdateTime = System.currentTimeMillis();
+    }
+    public void setTargetPower(double targetPower){
+        power = targetPower;
+    }
+    public double getPriority(){
+        if (power-lastPower == 0){
+            lastUpdateTime = System.currentTimeMillis();
+            return 0;
+        }
+        return basePriority + Math.abs(power-lastPower) * (System.currentTimeMillis() - lastUpdateTime) * priorityScale;
+    }
+    public void update(){
+        lastUpdateTime = System.currentTimeMillis();
+        lastPower = power;
+    }
+}
